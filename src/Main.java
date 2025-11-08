5ad4568eaffc953f6832b94e8c3d365e21790675
Daria Svoboda <anastasiaturbina480@gmail.com>
2025-11-08 09:30:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static class NotEnoughRollsException extends Exception {
+        public NotEnoughRollsException(String message) {
+            super(message);
+        }
+    }
+
+    public static class SushiValidator {
+        private static final int MIN_ROLLS = 20;
+
+        public static void checkRollsForParty(int rollsCount) throws NotEnoughRollsException {
+            if (rollsCount < MIN_ROLLS) {
+                throw new NotEnoughRollsException(
+                        "Всего " + rollsCount + " роллов? Подружкам и тебе нужно минимум " + MIN_ROLLS + "!"
+                );
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Сколько роллов вы заказали для тусни с подружками? ");
+        int rolls = scanner.nextInt();
+
+        try {
+            SushiValidator.checkRollsForParty(rolls);
+            System.out.println("Ну щас наедитесь вкусно, молодец!");
+            System.out.println("Приятного аппетита! Включите сериальчик и можно начинать.");
+        } catch (NotEnoughRollsException e) {
+            System.out.println("Ты чего?!");
+            System.out.println(e.getMessage());
+            System.out.println("Это мало, ещё заказывай!");
+        }
+    }
+}
